caedbaab05e33ae9080b90e24026871e9aa5cdbc
Tracyaa <tracyho0867@gmail.com>
2019-08-09 14:39:34 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,20 +42,15 @@ end
 # school: Faker::University.name,
 
 
-User.create(name: 'Tom', email: 'tom@tom.com', password: '123', gender: 'other', age: 13, school:'Harvard')
-User.create(name: 'Floppy', email: 'floppy@floppy.com', password: '12345', gender: 'male', age: 12, school:'Harvard', has_a_post: true)
-User.create(name:'Potato', email:'potato@potato.com', password: '12345', gender:'female', age: 16, school:'Harvard')
-User.create(name:'Walt', email:'walt@walt.com', password: '12345', gender: 'female', age: 14, school:'MIT', has_a_post: true)
-User.create(name:'Jesse', email:'jesse@jesse.com', password: '12345', gender:'male', age: 15, school:'QC', has_a_post: true)
-User.create(name:'Maximo', email:'maximo@maximo.com', password: '12345', gender:'female', age: 15, school:'Island', has_a_post: true)
+User.create(name: 'Tom', email: 'tom@tom.com', password: '123', gender: 'other', age: 13, school:'Harvard', img_url: "https://www.outtherecolorado.com/wp-content/uploads/2017/03/e9bb2a136a67b3aa4f28fa89ff2e9e1f-1024x897.jpg")
+User.create(name: 'Floppy', email: 'floppy@floppy.com', password: '12345', gender: 'male', age: 12, school:'Harvard', has_a_post: true, img_url: "https://www.outtherecolorado.com/wp-content/uploads/2017/03/e9bb2a136a67b3aa4f28fa89ff2e9e1f-1024x897.jpg")
+User.create(name:'Potato', email:'potato@potato.com', password: '12345', gender:'female', age: 16, school:'Harvard', img_url: "https://www.outtherecolorado.com/wp-content/uploads/2017/03/e9bb2a136a67b3aa4f28fa89ff2e9e1f-1024x897.jpg")
+User.create(name:'Walt', email:'walt@walt.com', password: '12345', gender: 'female', age: 14, school:'MIT', has_a_post: true, img_url: "https://www.outtherecolorado.com/wp-content/uploads/2017/03/e9bb2a136a67b3aa4f28fa89ff2e9e1f-1024x897.jpg")
+User.create(name:'Jesse', email:'jesse@jesse.com', password: '12345', gender:'male', age: 15, school:'QC', has_a_post: true, img_url: "https://www.outtherecolorado.com/wp-content/uploads/2017/03/e9bb2a136a67b3aa4f28fa89ff2e9e1f-1024x897.jpg")
+User.create(name:'Maximo', email:'maximo@maximo.com', password: '12345', gender:'female', age: 15, school:'Island', has_a_post: true, img_url: "https://www.outtherecolorado.com/wp-content/uploads/2017/03/e9bb2a136a67b3aa4f28fa89ff2e9e1f-1024x897.jpg")
 User.create(name:'Andy', email:'andy@andy.com', password: '12345', gender:'male', age: 15, school:'Hunter')
 # starting 8
 load_doctors
-User.create(name:'Patrick', email:'patrick@patrick.com', password: '12345', gender:'male', age: 50, role: 'counselor', location:'New York')
-User.create(name:'Lyla', email:'lyla@lyla.com', password: '12345', gender:'female', age: 35, role: 'counselor', location:'New York')
-User.create(name:'Tony', email:'tony@tony.com', password: '12345', gender:'female', age: 29, role: 'counselor', location:'New York')
-User.create(name:'Greg', email:'greg@greg.com', password: '12345', gender:'other', age: 30, role: 'counselor', location:'New York')
-User.create(name:'Corgi', email:'corgi@corgi.com', password: '12345', gender:'male', age: 23, role: 'counselor', location:'New York')
 
 Post.create(student_id: 2, gender_preference: 'male', type: nil, content: 'I got bullied')
 Post.create(student_id: 4, counselor_id: 8, gender_preference: 'other', type: nil, content: "My parents are arguing everyday... I feel terrible.", requested: true, activated: true)
