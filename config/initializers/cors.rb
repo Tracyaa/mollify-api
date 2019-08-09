7a01977acfc7604a9d0531899c98dfb3ce534b74
Tracyaa <tracyho0867@gmail.com>
2019-08-09 00:00:12 -0400

    changed cors file back

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mollify.herokuapp.com'
+    origins '*'
 
-    resource 'https://mollify.herokuapp.com',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
